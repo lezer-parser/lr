37fb91ba6a42715ac807d2cde96dc36e18ee654a
Marijn Haverbeke <marijn@haverbeke.nl>
2020-09-23 09:52:43 +0200

    Upgrade dev dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import typescript from "rollup-plugin-typescript2"
-import nodeResolve from "rollup-plugin-node-resolve"
-import commonJS from "rollup-plugin-commonjs"
+import {nodeResolve} from "@rollup/plugin-node-resolve"
+import commonJS from "@rollup/plugin-commonjs"
 
 export default {
   input: "./src/index.ts",
