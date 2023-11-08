ddc7df975779c80f45ae4c1ad77d5209388689ba
illright <leva181777@gmail.com>
2023-11-08 14:43:56 +0100

    Reexport the enums from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export {LRParser, ParserConfig, ContextTracker} from "./parse"
 export {InputStream, ExternalTokenizer, LocalTokenGroup} from "./token"
 export {Stack} from "./stack"
+export {Action, Encode, File, ParseState, Seq, Specialize, StateFlag, Term} from "./constants"
